a41afb2774e5006b080eee3aab044d65e9477ce2
Deniel Jekyll <kuhtin.vasya@yandex.ru>
2018-02-07 22:55:53 +0700

    shapes transformation fixed

--- pure.zlo_2/Form1.Designer.cs
+++ pure.zlo_2/Form1.Designer.cs
@@ -247,6 +247,7 @@
             this.Controls.Add(this.dashbord);
             this.Controls.Add(this.glViewer);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.Name = "Canvas";
             this.Text = "pure.zlo_2";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Canvas_KeyDown);
